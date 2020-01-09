8fb27601dddd8917b3ebc1628136151e77fc8357
Martin Braun <martin.braun@ettus.com>
2020-01-09 11:13:02 -0800

    Reset repository (delete all files)

--- test-gnuradio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm \
-    --name gnuradio-test \
-    --net=host \
-    -e DISPLAY \
-    -v $HOME/.Xauthority:/home/user/.Xauthority \
-    gnuradio/gnuradio-python-minimal
